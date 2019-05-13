2d26cf0a7acc128758b041082e68e67e6e0f3164
Brandon Hanlon <give101k@gmail.com>
2019-05-13 15:51:07 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const googleMapsClient = require("@google/maps").createClient({
+  key: "AIzaSyCjdWc2whWGJLhGA_uZwaNEfjtsUo_BCHk",
+  Promise: Promise
+});
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+app.get("/place", (req, res) => {
+  googleMapsClient
+    .placesNearby({
+      language: "en",
+      
+      location: [41.133916, -74.356341],
+      radius: 30000,
+      type: "car_repair"
+    })
+    .asPromise()
+    .then(response => {
+      res.json(response.json.results);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get("/place/:lat/:long", (req, res) => {
+  googleMapsClient
+    .placesNearby({
+      language: "en",
+      location: [req.params.lat, req.params.long],
+      radius: 3000,
+      type: "car_repair"
+    })
+    .asPromise()
+    .then(response => {
+      res.json(response.json.results);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get("/photo/:ref", (req, res) => {
+  var refr = req.params.ref;
+
+  var photo = {
+    photoreference: refr,
+    maxwidth: 200,
+    maxheight: 200
+  };
+
+  googleMapsClient
+    .placesPhoto(photo)
+    .asPromise()
+    .then(response => {
+      res.json({ ur: response.requestUrl });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get("/zip/:zippy", (req, res) => {
+  googleMapsClient
+    .geocode({
+      address: req.params.zippy
+    })
+    .asPromise()s
+    .then(response => {
+      res.json(response.json.results[0].geometry.location);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("sever is listening on port 3001");
+});
